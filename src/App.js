951e481a9b60899339ab436e20a4e340027fe9bf
Emiliano Nava Morales <enava@asimov.mx>
2019-04-19 20:15:12 -0500

    [test] cambio de desarrollo

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Cambio de prueba </h1>
         </header>
         <p className="App-intro">
-          Welcome to this Docker Meetup with Jenkins
+          Welcome informacion 
 
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
